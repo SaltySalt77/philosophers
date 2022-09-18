9b6378f1c43afcb4f91bd6574700abd424bd25a1
hyna <hyna@c5r8s7.42seoul.kr>
2022-09-18 21:30:03 +0900

    refact : refacted to start at same time

--- philo/srcs/philo_routine.c
+++ philo/srcs/philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: hyna <hyna@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 20:42:58 by hyna              #+#    #+#             */
-/*   Updated: 2022/09/18 21:27:19 by hyna             ###   ########.fr       */
+/*   Updated: 2022/09/18 21:29:15 by hyna             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ static void	wait_stdtime(t_info	*info)
 	{
 		if (info->std_time != -1)
 			break ;
-		usleep(1);
 	}
 }
 // 시간 순 대로 프린트하도록 뮤텍스 락을 걸면 너무 시간이 지연됨.
@@ -35,11 +34,11 @@ void	*philo_routine(void	*value)
 	wait_stdtime(philo->info);
 	while (1)
 	{
-		pthread_mutex_lock(&(philo->info->print));
+		// pthread_mutex_lock(&(philo->info->print));
 		gettimeofday(&time, NULL);
 		printf("[%d] Started philo[%d]\n",
 			(time.tv_usec - philo->info->std_time), philo->name);
-		pthread_mutex_unlock(&(philo->info->print));
+		// pthread_mutex_unlock(&(philo->info->print));
 		break ;
 		//take_fork()
 		//think()
